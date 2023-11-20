16976b133bc3424dfc73419b4892c939204c186a
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-11-20 13:52:01 +0000

    Created dynamic header

---
+++ scripts/header.js
@@ -0,0 +1,20 @@
+const headerHTML = `
+        <header>
+        <nav class="nav-bar">
+            <div class="logo">
+                <img src="assets/images/logo.png" alt="logo">
+            </div>
+            <div class="nav-links">
+                    <a href="index.html">HOME</a>
+                    <a href="about.html">ABOUT</a>
+                    <a href="faculty.html">FACULTY</a>
+                    <a href="research-groups.html">RESEARCH GROUPS</a>
+            </div>
+            <div class="nav-icon">
+                <img src="assets/images/menu-icon.svg" alt="menu-icon" >
+            </div>
+        </nav>     
+    </header>
+`;
+
+document.body.insertAdjacentHTML("afterbegin", headerHTML);
