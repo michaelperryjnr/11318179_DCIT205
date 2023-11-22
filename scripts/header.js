04d2e0d3a19ede97ca3b3a3057e9b4e9bb227e02
Michael Perry Tettey <michaelperryt97@gmail.com>
2023-11-22 16:15:22 +0000

    Minor changes

--- scripts/header.js
+++ scripts/header.js
@@ -4,17 +4,31 @@ const headerHTML = `
             <div class="logo">
                 <img src="assets/images/logo.png" alt="logo">
             </div>
-            <div class="nav-links">
-                    <a href="index.html">HOME</a>
-                    <a href="about.html">ABOUT</a>
-                    <a href="faculty.html">FACULTY</a>
-                    <a href="research-groups.html">RESEARCH GROUPS</a>
-                    <a href="events.html">EVENTS</a>
+            <ul class="nav-links">
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="faculty.html">FACULTY</a></li>
+                <li><a href="research-groups.html">RESEARCH GROUPS</a></li>
+                <li><a href="events.html">EVENTS</a></li>  
+            </ul>
+            <div class="nav-icon" id="menu-btn">
+                <img src="assets/images/menu-icon.svg" alt="menu-icon">
             </div>
-            <div class="nav-icon">
-                <img src="assets/images/menu-icon.svg" alt="menu-icon" >
+            <div class="mobile-nav-links-list mobile-nav-links" id="mobile-nav-links-list">
+                <div class="mobile-nav-img-container">
+                    <div>
+                        <img class="menu-close" id="close-btn" src="assets/images/menu-close.svg" alt="close-icon">
+                    </div>
+                </div>
+                <ul class="mobile-list">
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="faculty.html">FACULTY</a></li>
+                    <li><a href="research-groups.html">RESEARCH GROUPS</a></li>
+                    <li><a href="events.html">EVENTS</a></li>
+                </ul>
             </div>
-        </nav>     
+        </nav>
     </header>
 `;
 
